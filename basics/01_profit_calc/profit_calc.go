bd7c458da9cbd043b73d0579377dd27823e261c0
YoussefKhaildAli <yossofwd@gmail.com>
2026-06-22 09:12:13 +0300

    chore: defo better error handling.

--- basics/01_profit_calc/profit_calc.go
+++ basics/01_profit_calc/profit_calc.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	revenue := input("revenue")
-	expenses := input("expenses")
-	taxRate := input("tax rate")
+	revenue, err := input("revenue")
+	handleError(err)
+	expenses, err := input("expenses")
+	handleError(err)
+	taxRate, err := input("tax rate")
+	handleError(err)
 
 	earningsBeforeTax, earningsAfterTax, ratio := earnings(revenue, expenses, taxRate)
 
@@ -20,15 +25,24 @@ func main() {
 	fmt.Printf("ratio: %.2f", ratio)
 }
 
-func input(entry string) float64 {
+func input(entry string) (float64, error) {
 	var entryValue float64
 	fmt.Printf("Enter %v:", entry)
 	fmt.Scan(&entryValue)
-	return entryValue
+
+	if entryValue <= 0 {
+		return 0, errors.New("Invalid inuput. Must be greater than 0")
+	}
+	return entryValue, nil
 }
 
 func earnings(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	eat := ebt - (ebt / taxRate)
 	return ebt, eat, ebt / eat
+}
+
+func handleError(err error) {
+	println(err.Error())
+	os.Exit(1)
 }
