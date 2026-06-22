626b1f547556770239e62ed4ba844e7404f54f00
YoussefKhaildAli <yossofwd@gmail.com>
2026-06-22 08:05:27 +0300

    chore: Use functions.

--- basics/01_profit_calc/profit_calc.go
+++ basics/01_profit_calc/profit_calc.go
@@ -5,24 +5,30 @@ import (
 )
 
 func main() {
-	var revenue, expenses, taxRate float64
 
-	print("Enter revenue: ")
-	fmt.Scan(&revenue)
+	revenue := input("revenue")
+	expenses := input("expenses")
+	taxRate := input("tax rate")
 
-	print("Enter expenses: ")
-	fmt.Scan(&expenses)
-
-	print("Enter tax rate: ")
-	fmt.Scan(&taxRate)
-
-	earningsBeforeTax := revenue - expenses
-	earningsAfterTax := earningsBeforeTax - (earningsBeforeTax / taxRate)
+	earningsBeforeTax, earningsAfterTax, ratio := earnings(revenue, expenses, taxRate)
 
 	fmt.Printf("Earnings before tax: %v \n", earningsBeforeTax)
 
 	fmt.Printf("Type of Earnings after tax:  %T \n", earningsAfterTax)
 	fmt.Printf("Earnings after tax:  %#v \n", earningsAfterTax)
 
-	fmt.Printf("ratio: %.2f", earningsBeforeTax/earningsAfterTax)
+	fmt.Printf("ratio: %.2f", ratio)
+}
+
+func input(entry string) float64 {
+	var entryValue float64
+	fmt.Printf("Enter %v:", entry)
+	fmt.Scan(&entryValue)
+	return entryValue
+}
+
+func earnings(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	eat := ebt - (ebt / taxRate)
+	return ebt, eat, ebt / eat
 }
